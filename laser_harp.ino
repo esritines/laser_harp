3870ef3c5f7ce864f9772b974cbc61917a9172ad
Walberto Ibarra <waldo.ibarra92@gmail.com>
2016-11-27 18:16:28 -0600

    undefined

--- laser_harp.ino
+++ laser_harp.ino
@@ -1,39 +1,44 @@
 #include "songs.h"
 
-const byte MIDI_NOTE_OFF = 0b10000000;
-const byte MIDI_NOTE_ON = 0b10010000;
+// Number of strings the harp has.
 const short STRINGS = 16;
 
+const short BAUD_RATE = 19200;
+const short DEMO_PIN = 16;
+const byte MIDI_NOTE_OFF = 0b10000000;
+const byte MIDI_NOTE_ON = 0b10010000;
+
 // do, si, la, sol, fa, mi, re... (and so on)
-short tones[STRINGS] = {0, 11, 9, 7, 5, 4, 2, 12, 23, 21, 19, 17, 16, 14, 24, 35};
-short photoresistors[STRINGS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+const short NOTES[STRINGS] = {0, 11, 9, 7, 5, 4, 2, 12, 23, 21, 19, 17, 16, 14, 24, 35};
+const short PHOTORESISTORS[STRINGS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+const short LEDS[STRINGS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
 float photoReceptorValues[STRINGS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 float photoReceptorVoltage[STRINGS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-short leds[STRINGS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-
 bool playStrings[STRINGS] = {true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true};
 
-bool playDemo = false;
+// Variable for reading the push button status.
+int playDemo = 0;
 
 void setup() {
-	Serial.begin(19200);
+	Serial.begin(BAUD_RATE);
 
 	for (short i = 0; i < STRINGS; i++) {
-		pinMode(leds[i], OUTPUT);
-		pinMode(photoresistors[i], INPUT);
+		pinMode(LEDS[i], OUTPUT);
+		pinMode(PHOTORESISTORS[i], INPUT);
 
 		// Turn off all the LEDs.
-		digitalWrite(leds[i], LOW);
+		digitalWrite(LEDS[i], LOW);
 	}
 
-	// Switch to activate demo mode.
-	pinMode(16, INPUT);
+	// Push button to activate demo mode.
+	pinMode(DEMO_PIN, INPUT);
 }
 
 void loop() {
-	playDemo = digitalRead(16);
+	playDemo = digitalRead(DEMO_PIN);
 
-	if (playDemo)
+	if (playDemo == HIGH)
 		demoMode(&demo);
 	else
 		normalMode();
@@ -43,8 +48,8 @@ void demoMode(struct Song *demo) {
 	// Abraham pls, remember we discussed about this loop.
 	// Stop all the current playing sounds.
 	for (short i = 0; i < STRINGS; i++) {
-		digitalWrite(leds[i], LOW);
-		midiMessage(MIDI_NOTE_OFF, tones[i]);
+		digitalWrite(LEDS[i], LOW);
+		midiMessage(MIDI_NOTE_OFF, NOTES[i]);
 	}
 
 	//Serial.println("Playing %s.", demo->name);
@@ -66,38 +71,38 @@ void normalMode() {
 		// Check if laser is not interrupted.
 		if (playStrings[i]) {
 			// Read value of photoresistor and convert to voltage value.
-			photoReceptorValues[i] = analogRead(photoresistors[i]);
+			photoReceptorValues[i] = analogRead(PHOTORESISTORS[i]);
 			photoReceptorVoltage[i] = photoReceptorValues[i] * (5.0 / 1023.0);
 
 			// If the string is interrupted stop the MIDI note.
 			if (photoReceptorVoltage[i] > 1.0) {
-				digitalWrite(leds[i], HIGH);
-				midiMessage(MIDI_NOTE_OFF, tones[i]);
+				digitalWrite(LEDS[i], HIGH);
+				midiMessage(MIDI_NOTE_OFF, NOTES[i]);
 				playStrings[i] = false;
 			}
 		} else {
 			// Read value of photoresistor and convert to voltage value.
-			photoReceptorValues[i] = analogRead(photoresistors[i]);
+			photoReceptorValues[i] = analogRead(PHOTORESISTORS[i]);
 			photoReceptorVoltage[i] = photoReceptorValues[i] * (5.0 / 1023.0);
 
 			// If the string is not interrupted play a MIDI note.
 			if (photoReceptorVoltage[i] < 1.0) {
-				digitalWrite(leds[i], LOW);
-				midiMessage(MIDI_NOTE_ON, tones[i]);
+				digitalWrite(LEDS[i], LOW);
+				midiMessage(MIDI_NOTE_ON, NOTES[i]);
 				playStrings[i] = true;
 			}
 		}
 	}
 }
 
-/*
- * Manda un mensaje serial en formato MIDI
- * @param command Un comando MIDI
- * @param MIDInote La nota para el comando anterior
- * @param MIDIvelocity (0~127) establece el volumen de la nota
-*/
+// Sends a serial message in MIDI format.
 void midiMessage(uint8_t command, uint8_t MIDInote) {
+	// Whether to play or stop the note.
 	Serial.write(command);
+
+	// Note to play or stop.
 	Serial.write(MIDInote);
+
+	// Note volume.
 	Serial.write(127);
 }
